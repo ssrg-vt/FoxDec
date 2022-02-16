85a4d93593413a5fd977c5b1ba9efd8e9b7cc5de
Freek Verbeek <freek@vt.edu>
2022-02-16 06:07:30 -0500

    Updating script for running examples

--- foxdec/examples/script.sh
+++ foxdec/examples/script.sh
@@ -8,17 +8,17 @@ do
     # for the .entry file in the subdir
 
     # wait while max num of subprocesses is reached
-    while [ `jobs | wc -l` -ge 4 ]
-    do
-      sleep 10
-    done
+    #while [ `jobs | wc -l` -ge 1 ]
+    #do
+    #  sleep 10
+    #done
 
     # extract basename
     file=${f##*/} 
     base=${file%.*}
 
     echo "Running example $d/$base"
-    { time foxdec-exe 0 $d $base ; echo "$d/$base done." > /dev/tty ; } &> $d/$base.out &
+    { time foxdec-exe 0 $d $base ; echo "$d/$base done." > /dev/tty ; } &> $d/$base.out 
   done
 done
 
