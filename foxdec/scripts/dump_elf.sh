dc725732f1183e41c098a76f5de2f1546588e4d7
Freek Verbeek <freek@vt.edu>
2022-08-26 12:59:43 +0200

    Adding .data.rel.ro section

--- foxdec/scripts/dump_elf.sh
+++ foxdec/scripts/dump_elf.sh
@@ -125,6 +125,11 @@ do
       found_section=true;
       found_relevant_section=true;
    fi
+   if [[ $line == *".data.rel.ro"* ]]; then
+      current_sect_name=".data.rel.ro"
+      found_section=true;
+      found_relevant_section=true;
+   fi
 
    if [ "$found_relevant_section" = true ] ; then
       # dump section, remove empty lines, remove lines with "Hex dump", trim spaces (leading and end), remove double spaces, takes columns 1 to 5
