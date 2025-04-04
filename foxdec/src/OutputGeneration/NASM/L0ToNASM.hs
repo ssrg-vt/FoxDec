639275946917ea499e3fb60d47e44f50b66b001f
freekver <verbeek.freek@gmail.com>
2025-04-04 10:09:07 +0200

    Jump tables in NASM are now in a .data.rel.ro section instead of a .rodata section

--- foxdec/src/OutputGeneration/NASM/L0ToNASM.hs
+++ foxdec/src/OutputGeneration/NASM/L0ToNASM.hs
@@ -1021,7 +1021,7 @@ mk_jump_table l (entry,cfg,(a,inds)) = concatMap mk $ S.toList inds
     , "resb 8"
     , show (label_jump_table_temp_storage entry a 1) ++ ":"
     , "resb 8"
-    , "section .rodata"
+    , "section .data.rel.ro"
     , show (label_jump_table_redirect_data entry a) ++ ":"]
     ++
     map mk_entry (sortBy (compare `on` fst) $ IM.assocs tbl)
