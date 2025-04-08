185350345520f3a46870a2f460a9f25a8274e3d7
Basavesh Shivakumar <basavesh.shivakumar@gmail.com>
2025-04-08 13:12:25 -0400

    add add2numbers for examples

---
+++ foxdec/examples/add2numbers/add2numbers.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int sum;
+
+int main() {
+    int a = 5;
+    int b = 3;
+    sum = a + b;
+    printf("The sum of %d and %d is %d\n", a, b, sum); // Print the result
+    return sum; // Return the sum of a and b
+}
