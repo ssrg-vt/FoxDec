d0bdc93fdf6f9a700fcae27572699e3f0c5a9363
Daniel Engel <daniel.engel@ou.nl>
2025-10-27 14:35:14 +0000

    CI Setup

--- foxdec/ehframe_reader/ellf_cfi_patch.py
+++ foxdec/ehframe_reader/ellf_cfi_patch.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 """
 reconstruct_cfi_to_asm.py
 
--- foxdec/src/Binary/Elf.hs
+++ foxdec/src/Binary/Elf.hs
@@ -539,11 +539,3 @@ split_bytestring bs n
       (bs0,bs1) -> bs0 : split_bytestring bs1 n
 
 
-
-
-{-# INLINABLE (!?) #-}
-xs !? n
-  | n < 0     = Nothing
-  | otherwise = foldr (\x r k -> case k of
-                                   0 -> Just x
-                                   _ -> r (k-1)) (const Nothing) xs n
