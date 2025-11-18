ad95af5455bcbbf7373611fb1b2187e181699684
freekver <verbeek.freek@gmail.com>
2025-11-18 13:39:32 +0100

    Updating building script for pipeline

--- ci/twelf-foxdec.py
+++ ci/twelf-foxdec.py
@@ -27,30 +27,13 @@ def cfifile_path(name):
 ## == SUBPROCESSES =============================================================
 ## =============================================================================
 
-def run_disasm(binary):
-    cmd = [DISASM_PROG, binary]
-    with open(xedfile_path(binary), "w") as f:
-        subprocess.run(cmd, stdout=f)
-
-def run_ellf_cfi_patch(binary):
-    cmd = [CFI_PATCH_PROG, "generate", "-o", cfifile_path(binary), binary]
-    subprocess.run(cmd, stdout=subprocess.DEVNULL)
-
 def run_foxdec(binary, config, directory):
     cmd = [FOXDEC_PROG, '-c', config, '-i', 'ELLF', '-d', directory, '-n', binary, '--Gellf']
     with open('foxdec.log', 'w') as f:
         f.write(f"RUNNING: {' '.join(cmd)}\n\n")
         subprocess.run(cmd, stdout=f)
 
-def _delete_file(path):
-    try:
-        os.remove(path)
-    except:
-        print(f"Failed to delete {path}")
 
-def cleanup(binary):
-    _delete_file(xedfile_path(binary))
-    _delete_file(cfifile_path(binary))
 
 ## =============================================================================
 ## == CHECK HEALTH =============================================================
@@ -86,11 +69,8 @@ def main():
         print("All arguments (--binary, --config, --directory) must be passed", sys.stderr)
         sys.exit(1)
 
-    run_disasm(args.binary)
-    run_ellf_cfi_patch(args.binary)
     run_foxdec(args.binary, args.config, args.directory)
 
-    cleanup(args.binary)
 
 
 if __name__ == '__main__':
